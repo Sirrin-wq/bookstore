920668c014006551c85a70b3182067bbc305d044
Yan Bisyukov <yanbisyukov@gmail.com>
2024-10-28 14:35:50 +0300

    add ability for user to have multiple roles

--- src/main/java/com/example/bookstore/entity/user/Role.java
+++ src/main/java/com/example/bookstore/entity/user/Role.java
@@ -1,6 +1,21 @@
 package com.example.bookstore.entity.user;
 
-public enum Role {
-    ADMIN_ROLE,
-    CUSTOMER_ROLE
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
 }
